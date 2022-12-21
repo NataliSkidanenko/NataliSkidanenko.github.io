6d575b237604acae5cd49e009708827a3025deb9
NataliSkidanenko <sni.nata102@gmail.com>
2022-12-21 11:44:41 +0100

    Updated MNTN

--- mntn/js/menu.js
+++ mntn/js/menu.js
@@ -3,16 +3,18 @@
     const buttonOpenMenu = document.querySelector('#menu-open');
     const buttonCloseMenu = document.querySelector('#menu-close');
 
-    buttonOpenMenu.addEventListener('click', () => openMenu());
-    buttonCloseMenu.addEventListener('click', () => closeMenu());
+    buttonOpenMenu.addEventListener('click', openMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
 
-    function openMenu() {
+    function openMenu(e) {
+        e.preventDefault();
         modalMenu.classList.add('animation-in');
         modalMenu.style.display = 'flex';
         document.body.style.overflow = 'hidden';
     }
 
-    function closeMenu() {
+    function closeMenu(e) {
+        e.preventDefault();
         modalMenu.classList.remove('animation-in');
         modalMenu.classList.add('animation-out');
 
